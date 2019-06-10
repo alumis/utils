b41f6e7c27635f0dd2644253a7aadacbfb67cbe8
Emil Müller <emil@alumis.no>
2019-06-10 13:45:54 +0200

    no message

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export * from './src/observe';
 export * from './src/delayAsync';
 export * from "./src/getScrollParent";
 export * from "./src/elementIsVisible";
+export * from "./src/firstLetterCapitalized";
---
+++ src/firstLetterCapitalized.ts
@@ -0,0 +1,4 @@
+export function firstLetterCapitalized(str: string) {
+
+    return typeof str === "string" && 1 <= str.length ? str.charAt(0).toLocaleUpperCase() + str.substr(1) : str;
+}
