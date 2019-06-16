23f945b7f3b4573ed6bd85a70b70ff6290bdeac1
Emil Müller <emil@alumis.no>
2019-06-16 18:21:55 +0200

    no message

---
+++ src/loadImageAsync.ts
@@ -0,0 +1,27 @@
+﻿
+export function loadImageUrlAsync(imageElement: HTMLImageElement, url: string) {
+
+    return new Promise((resolve, reject) => {
+
+        let onLoad = () => {
+
+            imageElement.removeEventListener("load", onLoad);
+            imageElement.removeEventListener("error", onError);
+
+            resolve();
+        };
+
+        let onError = e => {
+
+            imageElement.removeEventListener("load", onLoad);
+            imageElement.removeEventListener("error", onError);
+
+            reject(e);
+        };
+
+        imageElement.addEventListener("load", onLoad);
+        imageElement.addEventListener("error", onError);
+
+        imageElement.src = url;
+    });
+}
