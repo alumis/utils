a12bb551b7d1b46e3b37ac51537980a346c5183a
Emil Müller <emil@alumis.no>
2019-08-16 16:51:45 +0200

    bumped

--- src/uint8ArrayToHex.ts
+++ src/uint8ArrayToHex.ts
@@ -1,7 +1,7 @@
-export function uint8ArrayToHex(uint8Array: Uint8Array) {
-    let hexadecimals: string[] = new Array<string>(length * 2);
-    for (let i = 0, length = uint8Array.length; i < length; ++i) {
-        let hexadecimal = (<Uint8Array>this)[i].toString(16);
+export function uint8ArrayToHex(data: Uint8Array) {
+    let hexadecimals: string[] = new Array<string>(data.length * 2);
+    for (let i = 0, length = data.length; i < length; ++i) {
+        let hexadecimal = data[i].toString(16);
         hexadecimals[i] = hexadecimal.length === 1 ? "0" + hexadecimal : hexadecimal;
     }
     return hexadecimals.join("");
--- src/whenAll.ts
+++
@@ -1,6 +0,0 @@
-
-export async function whenAll(tasks: Promise<any>[]) {
-
-    for (let t of tasks) 
-        await t;
-}
